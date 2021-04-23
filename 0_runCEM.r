f52cd3b38dad17dea62a660655f483ec36343cb2
Christoper Tracey <ctracey@paconserve.org>
2021-04-23 13:00:25 -0400

    beginnigns of the refactor

---
+++ 0_runCEM.r
@@ -0,0 +1,15 @@
+library(here)
+library(dismo)
+library(arcgisbinding)
+arc.check_product()
+library(ggplot2)
+
+
+
+
+sp_code <- "Lupinus perennis"
+
+
+source("1_PrepSpeciesData.r")
+source("1_PrepSpeciesData.r")
+source("1_PrepSpeciesData.r")
